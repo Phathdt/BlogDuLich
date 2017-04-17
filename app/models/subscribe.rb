3f6657a917430cecafff8ddb35b3237ca3c60e88
Phathdt <Phathdt.leviateam@gmail.com>
2017-04-17 20:00:32 +0700

    them 1 ti

--- app/models/subscribe.rb
+++ app/models/subscribe.rb
@@ -1,5 +1,5 @@
 class Subscribe < ApplicationRecord
 	validates :name, presence: true
 	validates :email, presence: true
-	validates :phone, presence: true ,length: { in: 6..20 }
+	validates :phone, presence: true ,length: { in: 6..21 }
 end
